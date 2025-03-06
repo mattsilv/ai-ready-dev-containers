c5dc281844d93e38cda395de7a8f5aac345f85ad
MS <156176+mattsilv@users.noreply.github.com>
2025-03-06 17:28:15 -0500

    Fix Vite build by adding index.html in root and updating config

--- demo/frontend/vite.config.js
+++ demo/frontend/vite.config.js
@@ -1,11 +1,19 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { resolve } from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
-    host: '0.0.0.0',
-    port: 3000
-  }
-})
+    host: "0.0.0.0",
+    port: 3000,
+  },
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html"),
+      },
+    },
+  },
+});
